efea22743c4489ea4d31695a18e7e592b5e7fa5b
amir <asarrafzadeh@wheretech.it>
2024-02-07 10:51:11 +0100

    basicString

---
+++ Basic/String/main.py
@@ -0,0 +1,105 @@
+# Define string variables
+str1 = "Hello World!, I am a Python Programmer"
+
+# Print the string
+print(str1)
+
+# Print the length of the string
+print(len(str1))
+
+# Print the first character of the string
+print(str1[0])
+
+# Print the last character of the string
+print(str1[-1])
+
+# Print the first 5 characters of the string
+print(str1[0:5])
+
+# Print the last 5 characters of the string
+print(str1[-5:])
+
+# Print the string in upper case
+print(str1.upper())
+
+# Print the string in lower case
+print(str1.lower())
+
+# Print the string in title case
+print(str1.title())
+
+# print in capital case
+print(str1.capitalize())
+
+# Print the string in reverse
+print(str1[::-1])
+
+# Split the string by a specific character
+print(str1.split(" ")) # Split by space
+print(str1.split(",")) # Split by comma
+
+# Replace a specific character in the string
+print(str1.replace("Python", "Java"))
+
+# Check if a specific character is in the string
+print("Python" in str1)
+
+# Check if a specific character is not in the string
+print("Python" not in str1)
+
+# Check if the string starts with a specific character
+print(str1.startswith("Hello"))
+
+# Check if the string ends with a specific character
+print(str1.endswith("Programmer"))
+
+# Count the number of times a specific character appears in the string
+print(str1.count("a"))
+
+# Find the index of a specific character in the string
+print(str1.find("Python"))
+
+# Find the index of a specific character in the string
+print(str1.index("Python"))
+
+# Check if the string is alphanumeric
+print(str1.isalnum())
+
+# Check if the string is alphabetic
+print(str1.isalpha())
+
+# Check if the string is a digit
+print(str1.isdigit())
+
+# Check if the string is a title
+print(str1.istitle())
+
+# Check if the string is a space
+print(str1.isspace())
+
+# Check if the string is a lower case
+print(str1.islower())
+
+# Check if the string is a upper case
+print(str1.isupper())
+
+# Check if the string is a printable
+print(str1.isprintable())
+
+# Check if the string is a identifier
+print(str1.isidentifier())
+
+# Check if the string is a decimal
+print(str1.isdecimal())
+
+# Check if the string is a numeric
+print(str1.isnumeric())
+
+# Check if the string is a ascii
+print(str1.isascii())
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Python
-Because I love to share what I know, in this repository I will try to share python scripts from 0 to advance scripts.
+Hi All, ![](https://user-images.githubusercontent.com/18350557/176309783-0785949b-9127-417c-8b55-ab5a4333674e.gif)
+============================================================================================================================
+# Enjoy Learning
+
+<img src="https://img2.juzaphoto.com/001/shared_files/uploads_hr/530128_large27865.jpg" alt="Photo">
+
+### Because I love to share what I know, in this repository I will try to share python scripts from 0 to advance scripts.
