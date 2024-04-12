7d70991cf9dffddb5ba4408e5777a65b8d672d78
AmirSarrafzadeh <sarrafzadeharasi.1871766@studenti.uniroma1.it>
2024-04-12 09:30:20 +0200

    tuple

---
+++ Basic/Tuple/main.py
@@ -0,0 +1,32 @@
+# Define a tuple
+myTuple = (1, 2, 3, "apple", "banana", "cherry")
+
+# Print the tuple
+print(myTuple)
+
+# Print the length of the tuple
+print(len(myTuple))
+
+# Print the first element of the tuple
+print(myTuple[0])
+
+# Print the last element of the tuple
+print(myTuple[-1])
+
+# Print the first 3 elements of the tuple
+print(myTuple[:3])
+
+# Print the last 3 elements of the tuple
+print(myTuple[-3:])
+
+# Print the tuple in reverse
+print(myTuple[::-1])
+
+# Concatenate two tuples
+anotherTuple = (4, 5, 6)
+concatenatedTuple = myTuple + anotherTuple
+print(concatenatedTuple)
+
+# Nested tuple
+nestedTuple = ((1, 2), ("apple", "banana"))
+print(nestedTuple)
