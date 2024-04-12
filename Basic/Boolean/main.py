660437e0c7d61fd49239572711a2c651c656be20
AmirSarrafzadeh <sarrafzadeharasi.1871766@studenti.uniroma1.it>
2024-04-12 09:38:55 +0200

    boolean

---
+++ Basic/Boolean/main.py
@@ -0,0 +1,26 @@
+# Define boolean variables
+bool1 = True
+bool2 = False
+
+# Print the boolean variables
+print(bool1)
+print(bool2)
+
+# Logical AND operation
+print(bool1 and bool2)
+
+# Logical OR operation
+print(bool1 or bool2)
+
+# Logical NOT operation
+print(not bool1)
+
+# Comparison operators
+print(5 > 3)
+print(5 < 3)
+print(5 == 5)
+print(5 != 3)
+print(5 >= 5)
+print(5 <= 3)
+print(5 in [1, 2, 3, 4, 5])
+print(5 not in [1, 2, 3, 4, 5])
